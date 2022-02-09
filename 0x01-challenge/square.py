973694cdd802d8cc1785c582480327ebf970b849
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-02-09 07:33:42 +0100

    Revert some updates to script

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -2,36 +2,21 @@
 """ A module for handling squares. """
 
 
-class Square:
-    """Represents a rectangle with equal sides."""
-    __width = 0
+class square:
+    """ Represents a rectangle with equal sides. """
+    width = 0
+    height = 0
 
     def __init__(self, *args, **kwargs):
         """ Initializes a new square. """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-    @property
-    def width(self):
-        """ Gets the width of this square. """
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """ Sets the width of this square. """
-        if type(value) is int or type(value) is float:
-            if value < 0:
-                raise ValueError("Value must be >= 0.")
-            else:
-                self.__width = value
-        else:
-            raise TypeError("Type must be an int or a float.")
-
-    def area(self):
+    def area_of_my_square(self):
         """ Computes the area of this square. """
         return self.width * self.width
 
-    def perimeter(self):
+    def PermiterOfMySquare(self):
         """ Computes the perimeter of this square. """
         return (self.width * 4)
 
@@ -41,7 +26,7 @@ class Square:
 
 
 if __name__ == "__main__":
-    s = Square(width=12, height=9)
+    s = square(width=12, height=9)
     print(s)
-    print(s.area())
-    print(s.perimeter())
+    print(s.area_of_my_square())
+    print(s.PermiterOfMySquare())
